eed0a77e0616fe997541348f722b76cb77326637
Noor Fatima <its.menoorfatima2010@gmail.com>
2026-07-18 12:25:03 +0500

    Create Variable_Data_Types.py

---
+++ Python/Variable_Data_Types.py
@@ -0,0 +1,20 @@
+name ="noor fatima"
+print("My name is",name)
+print(type(name))
+
+age=17
+print("My age is",age)
+
+city="karachi"
+print("I live in",city)
+
+course="python programming"
+print("My course is",course)
+
+Class = 9
+print("I'm in class",Class)
+
+print(type(city))
+print(type(course))
+print(type(Class))
+print(type(age))
